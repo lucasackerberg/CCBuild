bb9e6401400ee7aa66ac3e6d38c722fb470ca380
lucasackerberg <lucas@ackerberg.se>
2024-09-18 13:26:12 +0200

    moved createproject
    moved createproject

--- src/components/WrapperComponent/WrapperComponent.jsx
+++ src/components/WrapperComponent/WrapperComponent.jsx
@@ -1,9 +1,11 @@
 import ProjectComponent from "../ProjectComponent";
+import { CreateProject } from "../createProject/CreateProject";
 
 export default function WrapperComponent() {
     return(
         <div>
             <ProjectComponent></ProjectComponent>
+            <CreateProject></CreateProject>
         </div>
     )
 }
