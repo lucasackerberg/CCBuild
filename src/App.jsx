ad5c5af672ef5fcb7632e385c0e4bda06de1943f
OsborneLinus <linusholm1997@gmail.com>
2024-09-18 12:06:29 +0200

    Added createProject to submit data to database.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import './index.css';
-import { useState, useEffect } from 'react';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import ProjectComponent from './components/ProjectComponent';
-import supabase from './supabaseClient';
+import "./index.css";
+import { useState, useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import ProjectComponent from "./components/ProjectComponent";
+import supabase from "./supabaseClient";
 
 export default function App() {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
     const fetchSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setSession(session);
     };
 
     fetchSession();
 
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
 
@@ -25,16 +29,18 @@ export default function App() {
 
   const logout = async () => {
     await supabase.auth.signOut();
-    setSession(null);  // Reset session after logout
+    setSession(null); // Reset session after logout
   };
 
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />);
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
   } else {
     return (
       <div>
         <div>Logged in!</div>
-        <div><button onClick={logout}>Logout</button></div>
+        <div>
+          <button onClick={logout}>Logout</button>
+        </div>
         <ProjectComponent />
       </div>
     );
