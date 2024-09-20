57d59c1b41c2f81e7870d2ed1961ba61479a8ae5
lucasackerberg <lucas@ackerberg.se>
2024-09-20 10:29:41 +0200

    added product status
    added product_status table and added it in ProductList component for each product to have different quantities and statuses

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { useUser } from '../../contexts/UserContext';
 
-export const ProductList = ({ projectId }) => {
+export const ProductList = () => {
   const { products, loading } = useUser();
 
   if (loading) return <div>Loading...</div>;
   if (!products || products.length === 0) return <div>No products found.</div>;
 
-  // filtrering, kanske borde göra om? 
-  const filteredProducts = products.filter(product => product.project_id === projectId);
-
   return (
     <div>
       <ul>
-        {filteredProducts.map((product) => (
+        {products.map((product) => (
           <li key={product.id}>
             <h3>{product.name}</h3>
             <p>Description: {product.description}</p>
             <p>Aesthetic Condition: {product.aesthetic_condition}</p>
             <p>Functional Condition: {product.functional_condition}</p>
+            <h4>Status:</h4>
+            {product.product_status && product.product_status.length > 0 ? (
+              <ul>
+                {product.product_status.map((status, index) => (
+                  <li key={index}>
+                    <p>Status: {status.status}</p>
+                    <p>Quantity: {status.quantity}</p>
+                    <p>Market Status: {status.marknadsplatsen}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No status information available.</p>
+            )}
           </li>
         ))}
       </ul>
