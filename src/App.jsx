39d5f7719020c9ff52e2deffdea91242f241eede
sirisayshello <veckovis.salongen0p@icloud.com>
2024-09-24 10:54:44 +0200

    add header and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,33 @@ import RegisterComponent from './components/AuthComponents/RegisterComponent';
 import { UserProvider } from './contexts/UserContext';
 import ProjectList from './components/ProjectList/ProjectList';
 import ProductContainer from './components/ProductContainer/ProductContainer';
+import { Header } from './components/Header/Header';
 
 export default function App() {
   return (
     <UserProvider>
-      <Routes>
-        <Route
-          path="/register"
-          element={<RegisterComponent />}
-        />
-        <Route
-          path="/dashboard"
-          element={<WrapperComponent />}
-        />
-        <Route
-          path="/projects/:id"
-          element={<ProductContainer />}
-        />
-        <Route
-          path="/"
-          element={<LoginComponent />}
-        />{' '}
-        {/* Default route or landing page */}
-      </Routes>
+      <Header />
+      <div className="main">
+        <Routes>
+          <Route
+            path="/register"
+            element={<RegisterComponent />}
+          />
+          <Route
+            path="/dashboard"
+            element={<WrapperComponent />}
+          />
+          <Route
+            path="/projects/:id"
+            element={<ProductContainer />}
+          />
+          <Route
+            path="/"
+            element={<LoginComponent />}
+          />{' '}
+          {/* Default route or landing page */}
+        </Routes>
+      </div>
     </UserProvider>
   );
 }
