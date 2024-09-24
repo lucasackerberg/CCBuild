c4e79396d909d35dada1ef07ecd1884d48d2b944
sirisayshello <veckovis.salongen0p@icloud.com>
2024-09-24 11:38:43 +0200

    update bootstrap css

--- src/components/WrapperComponent/WrapperComponent.jsx
+++ src/components/WrapperComponent/WrapperComponent.jsx
@@ -6,16 +6,15 @@ export default function WrapperComponent() {
   const { profile, loading } = useUser();
 
   if (loading) {
-    return <p>Loading...</p>;  
+    return <p>Loading...</p>;
   }
 
   if (!profile) {
-    return <p>No profile found</p>;  
+    return <p>No profile found</p>;
   }
 
   return (
     <>
-      <h1>Welcome {profile.firstname}</h1>
       <ProjectComponent />
       <LogoutComponent></LogoutComponent>
     </>
