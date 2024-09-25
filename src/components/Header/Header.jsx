779e886ef664027371c469c17b5d90eaab1fc0c7
sirisayshello <veckovis.salongen0p@icloud.com>
2024-09-25 20:39:22 +0200

    redirect logo to dashboard

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import styles from './Header.module.css';
 import logo from '../../../src/assets/logo.webp';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <section>
-      <img
-        className={styles.logo}
-        src={logo}
-        alt="ccbuild logo"
-      />
+      <Link to={'/dashboard'}>
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="ccbuild logo"
+        />
+      </Link>
       <div className={styles.menu}>
         <input
           className={styles.search}
